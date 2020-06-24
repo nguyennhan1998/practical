b4329ffb18f652fcfa1b7d68ee0d4f5c9d668ac3
Nguyen nhan <nguyennnhanlovemyself@gmail.com>
2020-06-24 10:01:20 +0700

    nhan

--- app/Http/Controllers/Webcontroller.php
+++ app/Http/Controllers/Webcontroller.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Book;
 use App\Category;
 use App\Author;
+use App\Feedback;
 use App\User;
 use Illuminate\Http\Request;
 
@@ -26,6 +27,31 @@ class Webcontroller extends Controller
         return view("book.find", [
             "book"=>$book,
         ]);
+    }
+    public function surveyForm(){
+        return view("survey");
+    }
+    public function saveFeedback(Request $request){
+        $request->validate([
+            "name"=> "required|string|min:3",
+            "email"=>"required|string",
+            "phone"=>"required",
+            "feedback"=>"required|min:4"
+        ]);
+        try{
+            Feedback::create([
+                "name"=>$request->get("name"),
+                "email"=>$request->get("email"),
+                "phone"=>$request->get("phone"),
+                "feedback"=>$request->get("feedback"),
+            ]);
+
+//            $data["message"] = "thank for your feedback";
+//            notify("global","new_category",$data);
+        }catch (\Exception $exception){
+            return redirect()->back();
+        }
+        return redirect()->to("/");
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-
-
 Route::get("/list-book","WebController@listBook");
 Route::get("/find-book","WebController@findBook");
+Route::get("/survey","WebController@surveyForm");
+Route::post("/save-feedback","WebController@saveFeedback");
+
 
